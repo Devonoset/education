04e80c6f3989aeac5f24dbba0d1f440e1b96a439
Dmytro Podkopaev <devonoset@gmail.com>
2020-11-08 23:35:56 +0200

    Create index.js

---
+++ task1/index.js
@@ -0,0 +1,16 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+
+rl.question('Please enter the first number: ', (n) => {
+    const fx = (n) => +Math.max(...(n + '').split(''));
+    const fn = (n) => +Math.min(...(n + '').split(''));
+
+    console.log(fn(n)+fx(n)); 
+    rl.close();
+});
+
